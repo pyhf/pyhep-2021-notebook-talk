6037a9fbc75104f90c2c159c9304970f79705183
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-04 15:44:04 -0500

    add conda env setup scripts

--- expanse/create_expanse_funcx_env.sh
+++ expanse/create_expanse_funcx_env.sh
@@ -1,4 +1,4 @@
-echo "Setting up funcX Endpoint for pyhf"
+echo "Create funcX Endpoint for pyhf: pyhf-expanse-gpu"
 module purge
 module restore
 
@@ -6,6 +6,7 @@ module load gpu/0.15.4
 module load cuda/11.0.2
 module load anaconda3
 
+conda env create -f expanse-environment.yml
 conda activate pyhf-expanse-gpu
 
 module list
