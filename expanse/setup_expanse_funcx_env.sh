848ad699f46c0b00fd93da189c1888b63079f01c
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-04 15:40:14 -0500

    Update required module loads

--- expanse/setup_expanse_funcx_env.sh
+++ expanse/setup_expanse_funcx_env.sh
@@ -1,8 +1,11 @@
 echo "Setting up FuncX Endpoint for pyhf"
 module purge
 module restore
-module load cuda
+
+module load gpu/0.15.4
+module load cuda/11.0.2
 module load anaconda3
+
 conda activate pyhf-expanse-gpu
 
 module list
